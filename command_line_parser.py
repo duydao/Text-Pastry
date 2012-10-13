76018d1c59264c402ac939c26d2537ee340e71d5
Duy Dao <duy.dao@xivia.ch>
2012-10-13 18:29:33 +0200

    moved parser to separate file

--- command_line_parser.py
+++ command_line_parser.py
@@ -4,10 +4,58 @@ class CommandLineParser:
 	def CommandLineParser():
 		sublime.status_message("Creating CommandLineParser")
 
-	def run(command):
-		if not command: return None
-		
+	def parse(text):
+		if not text: return None
+		result = None
+
 		# start pasing the command string
-		
+        if text:
+            m1 = re.compile('(-?\d+) (-?\d+) (\d+)').match(text)
+            m2 = re.compile('\\\\i(\d*)(,(-?\d+))?').match(text)
+            m3 = re.compile('\\\\i\((\d*)(,(-?\d+))?\)').match(text)
+
+            m4 = re.compile('\\\\p\((.*?)\)').match(text)
+            if m1:
+                (current, step, padding) = map(str, text.split(" "))
+
+                TextPastryHistory.save_history("insert_nums", text)
+                sublime.status_message("Inserting Nums: " + text)
+                result = dict(Command="insert_nums", args={"current" : current, "step" : step, "padding" : padding})
+
+            elif m2 or m3:
+                m = None
+                if m2: m = m2
+                else: m = m3
+                current = m.group(1)
+                step = m.group(3)
+
+                if not current: current = "1"
+                if not step: step = "1"
+
+                TextPastryHistory.save_history("insert_nums", text)
+                sublime.status_message("Inserting #" + text)
+                result = dict(Command="insert_nums", args={"current" : current, "step" : step, "padding" : "1"})
+
+            elif text == "\\p":
+                TextPastryHistory.save_history("insert_text", text=sublime.get_clipboard(), label=text)
+                sublime.status_message("Inserting from clipboard")
+                result = dict(Command="insert_text", args={"text": sublime.get_clipboard()})
+
+            elif m4:
+                separator = m4.group(1)
+                if not separator: separator = None
+
+                TextPastryHistory.save_history("insert_text", text=sublime.get_clipboard(), label=text, separator=separator)
+                sublime.status_message("Inserting from clipboard with separator: " + str(separator))
+                result = dict(Command="insert_text", args={"text": sublime.get_clipboard(), "separator": separator})
+            
+            else:
+                sublime.status_message("Inserting " + text)
+                result = dict(Command="insert_text", args={"text": text})
+
+		return result
+
+	def valid():
+		pass
 
 
